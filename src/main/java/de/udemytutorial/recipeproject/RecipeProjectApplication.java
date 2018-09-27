fdc55552c2973aea299bbfb5433e25de66e87bdc
Jens Wambach <jens.wambach@t-online.de>
2018-09-27 08:27:41 +0200

    initial project commit

---
+++ src/main/java/de/udemytutorial/recipeproject/RecipeProjectApplication.java
@@ -0,0 +1,12 @@
+package de.udemytutorial.recipeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeProjectApplication.class, args);
+    }
+}
